0a029a6bb7cba737a2508f7170e2b261a6c485c2
jo <jollle44@gmail.com>
2022-01-14 23:27:44 +0100

    improve norvig spelling

--- Norvig/norvig_spelling.py
+++ Norvig/norvig_spelling.py
@@ -1,3 +1,4 @@
+#Author: Joëlle Gasser
 import re
 from collections import Counter
 
@@ -10,14 +11,18 @@ def lower_text(text):
 
 #dict counting the number of each word in the text file
 tokenized_words = Counter(lower_text(open('bncwwri_untag.txt').read()))
-# print(tokenized_words)
-#print(len(tokenized_words)) #376715 different words
-#print(sum(tokenized_words.values())) #appearing 89376867 times in the text
+#threshold for known words to appear at least x times
+known_words = Counter({k: c for k, c in tokenized_words.items() if c >=19})
+
+#print(tokenized_words)
+#print(known_words)
+#print(len(tokenized_words)) 
+#print(sum(tokenized_words.values()))
 #print(tokenized_words.most_common(10)) 
 
-def P(word, N=sum(tokenized_words.values())): 
+def P(word, N=sum(known_words.values())): 
     "Probability of `word`."
-    return tokenized_words[word] / N
+    return known_words[word] / N
 
 def correction(word): 
     "Most probable spelling correction for word."
@@ -31,15 +36,10 @@ def edits1(word):
     "All edits that are one edit away from `word`"
     letters    = 'abcdefghijklmnopqrstuvwxyz'
     splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
-    # print(splits)
     deletes    = [L + R[1:]               for L, R in splits if R]
-    # print(deletes)
     transposes = [L + R[1] + R[0] + R[2:] for L, R in splits if len(R)>1]
-    # print(transposes)
     replaces   = [L + c + R[1:]           for L, R in splits if R for c in letters]
-    # print(replaces)
     inserts    = [L + c + R               for L, R in splits for c in letters]
-    # print(inserts)
     return set(deletes + transposes + replaces + inserts)
 
 def edits2(word): 
@@ -48,20 +48,25 @@ def edits2(word):
 
 def known(words): 
     "The subset of `words` that appear in the dictionary of words"
-    return set(w for w in words if w in tokenized_words)
+    return set(w for w in words if w in known_words)
 
 #print(correction('korrectud'))
-print(edits1('smoothe'))
-#print(known(edits1('somthing')))
+#print(edits1('smoothe'))
+#print(correction('sunne'))
+#print(correction('smoothe'))
+#print(correction('anely'))
+#print(correction('legg'))
+#print(correction('legge'))
+#print(correction('aires'))
+#print(correction('frighted'))
+#print(correction('signe'))
+#print(correction("ev'ry"))
 
 def unit_tests():
-    assert correction('dresed') == 'dressed'              # insert
+    assert correction('dresed') == 'dressed'                # insert
     assert correction('korrectud') == 'corrected'           # replace 2
     assert correction('hauing') == 'having'                 # replace
-    #assert correction('frighted') == 'frightened'           # insert 2
-    assert correction('smoothe') == 'smooth'                    # delete
-    #assert correction('anely') == 'only'                    #delete and replace
-    #assert correction('sunne') == 'sun'
+    assert correction('smoothe') == 'smooth'                # delete
     assert correction('peotry') =='poetry'                  # transpose
     assert correction('peotryy') =='poetry'                 # transpose + delete
     assert correction('word') == 'word'                     # known
@@ -71,7 +76,7 @@ def unit_tests():
            Counter({'123': 1, 'a': 2, 'is': 2, 'test': 2, 'this': 2}))
     assert len(tokenized_words) == 376715
     assert sum(tokenized_words.values()) == 89376867
-    assert tokenized_words.most_common(10) == [
+    assert known_words.most_common(10) == [
      ('the', 5647035),
      ('of', 2877671),
      ('to', 2371089),
@@ -82,12 +87,11 @@ def unit_tests():
      ('is', 887134),
      ('for', 814558),
      ('it', 802975)]
-    assert tokenized_words['the'] == 5647035
-    #assert P('quintessential') == 0
+    assert known_words['the'] == 5647035
     assert 0.06 < P('the') < 0.07
     return 'unit_tests pass'
 
-#print(P('the')) #0.06318228854452909
+#print(P('the'))
 print(unit_tests())
 
 def spelltest(tests, verbose=False):
@@ -98,11 +102,14 @@ def spelltest(tests, verbose=False):
         w = correction(wrong)
         good += (w == right)
         if w != right:
-            unknown += (right not in tokenized_words)
+            unknown += (right not in known_words)
             if verbose:
                 print('correction({}) => {} ({}); expected {} ({})'
-                      .format(wrong, w, tokenized_words[w], right, tokenized_words[right]))
+                      .format(wrong, w, known_words[w], right, known_words[right]))
     print('{:.0%} of {} correct ({:.0%} unknown) '
+          .format(good / n, n, unknown / n, n))
+    with open('results_norvig.txt', 'w') as f:
+        f.write('{:.0%} of {} correct ({:.0%} unknown) '
           .format(good / n, n, unknown / n, n))
     
 def Testset(lines):
@@ -112,5 +119,5 @@ def Testset(lines):
             for wrong in wrongs.split()]
 
 
-spelltest(Testset(open('dev.txt'))) # Development set
+#spelltest(Testset(open('dev.txt'))) # Development set
 #spelltest(Testset(open('test.txt'))) # Final test set
