d08a4beb267610745d0b0c51f051c3eb49190b95
jo0704 <56045665+jo0704@users.noreply.github.com>
2022-05-06 10:44:59 +0200

    correct validation script

--- Nematus/scripts/validate.sh
+++ Nematus/scripts/validate.sh
@@ -7,8 +7,7 @@ nematus_home="/home/user/gasser/anaconda3/nematus" # path to nematus directory
 script_dir="/home/user/gasser/scripts_copy" # path to scripts directory
 data_dir="/home/user/gasser/norm" # path to data directory
 
-# ref="dev.ref"
-ref="test.trg.raw"
+ref="dev.ref"
 
 #compute chrF score
 $script_dir/postprocess.sh < $translations | sacrebleu --force -w 2 -m chrf -b $data_dir/$ref
